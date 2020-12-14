e73cc7ebb1826adde9839d81002257ef4ef2c49a
perezj253 <75589249+perezj253@users.noreply.github.com>
2020-12-13 23:36:08 -0600

    Update algorithms.py

--- algorithms.py
+++ algorithms.py
@@ -9,7 +9,7 @@ from functions import min_cost_edge, prims_initialize, is_spanning, cost, E
 from drawing import show_weighted_graph, draw_subtree
 
 
-def prims(G, starting_vertex, show_graph = False, show_cost = False):
+def prims(G, starting_vertex, show_steps = False):
     """Makes use of the helper functions in functions.py to implement Prim's
     algorithm on user specified graph. 
     
@@ -25,23 +25,21 @@ def prims(G, starting_vertex, show_graph = False, show_cost = False):
         The minimum spanning tree of a weigthed graoh.
 
     """
-    if show_graph == True:
+    if show_steps == True:
         show_weighted_graph(G)
         
     T = prims_initialize(G, starting_vertex)
     
-    if show_graph == True:
+    if show_steps == True:
         show_weighted_graph(G)
         
     while is_spanning(G, T) == False:
         e = min_cost_edge(G, T)
         T.add_edge(e[0], e[1])
-        if show_graph == True:
-            show_weighted_graph(G)
+        if show_steps == True:
+            draw_subtree(G, T)
             
-        if show_cost == True:
-            c = sum([cost(G, e) for e in E(T)])
-            print(f'The cost of this spanning tree is {c}')    
+  
             
     return T
     
